a43f6f864f28d787e8729b622475fdad2f9145d4
Karolis Matuliauskas <karolis@eddytravels.com>
2022-06-01 16:54:39 +0300

    TAD-49 Add Azerbaijani ('AzAZ') locale

---
+++ format_az_az.go
@@ -0,0 +1,55 @@
+package monday
+
+// ============================================================
+// Format rules for "az_AZ" locale: Azerbaijani (Azerbaijan)
+// ============================================================
+
+var longDayNamesAzAZ = map[string]string{
+	"Sunday":    "bazar",
+	"Monday":    "bazar ertəsi",
+	"Tuesday":   "çərşənbə axşamı",
+	"Wednesday": "çərşənbə",
+	"Thursday":  "cümə axşamı",
+	"Friday":    "cümə",
+	"Saturday":  "şənbə",
+}
+
+var shortDayNamesAzAZ = map[string]string{
+	"Sun": "B.",
+	"Mon": "B.E.",
+	"Tue": "Ç.A.",
+	"Wed": "Ç.",
+	"Thu": "C.A.",
+	"Fri": "C.",
+	"Sat": "Ş.",
+}
+
+var longMonthNamesAzAZ = map[string]string{
+	"January":   "yanvar",
+	"February":  "fevral",
+	"March":     "mart",
+	"April":     "aprel",
+	"May":       "may",
+	"June":      "iyun",
+	"July":      "iyul",
+	"August":    "avqust",
+	"September": "sentyabr",
+	"October":   "oktyabr",
+	"November":  "noyabr",
+	"December":  "dekabr",
+}
+
+var shortMonthNamesAzAZ = map[string]string{
+	"Jan": "yan",
+	"Feb": "fev",
+	"Mar": "mar",
+	"Apr": "apr",
+	"May": "may",
+	"Jun": "iyn",
+	"Jul": "iyl",
+	"Aug": "avq",
+	"Sep": "sen",
+	"Oct": "okt",
+	"Nov": "noy",
+	"Dec": "dek",
+}
